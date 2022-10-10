444245329851d83589a8e32d348245b3f3073597
fexxor <fexxor@users.noreply.github.com>
2022-10-10 18:57:37 +0200

    remove placeholder test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-// TODO: Write actual tests
-test("renders navbar links", () => {
-  render(<App />);
-  expect(screen.getByText(/Säkra, Förvara, Beskydda/i)).toBeInTheDocument();
-});
