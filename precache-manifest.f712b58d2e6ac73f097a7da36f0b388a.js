e8f0f1ab2ede00cf00b0f7e84ee14ea12a598273
fexxor <fexxor@users.noreply.github.com>
2022-10-10 17:17:58 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.f712b58d2e6ac73f097a7da36f0b388a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b68bf74719e34827a04d",
+    "url": "/scp-app/static/js/runtime~main.b68bf747.js"
+  },
+  {
+    "revision": "0a11be48e2026e9cba91",
+    "url": "/scp-app/static/js/main.0a11be48.chunk.js"
+  },
+  {
+    "revision": "1121a9ec46af122e3149",
+    "url": "/scp-app/static/js/2.1121a9ec.chunk.js"
+  },
+  {
+    "revision": "655ced442bf892c5e859",
+    "url": "/scp-app/static/js/1.655ced44.chunk.js"
+  },
+  {
+    "revision": "0a11be48e2026e9cba91",
+    "url": "/scp-app/static/css/main.e7450c95.chunk.css"
+  },
+  {
+    "revision": "46dc376442c0204bfa7a1df43195ae98",
+    "url": "/scp-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/scp-app/precache-manifest.f712b58d2e6ac73f097a7da36f0b388a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/scp-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
